29143a80d4870b26993985ab9181461f935110de
bee <bnesbitt355@gmail.com>
2025-05-08 21:11:08 +1000

    updates

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 
 import pygame
+from constants import *
 
 def main():
     print("Starting Asteroids!")
+    pygame.init():
 
 if __name__ == "__main__":
     main()
+
+print("Screen width:", SCREEN_WIDTH)
+print("Screen height:", SCREEN_HEIGHT)
